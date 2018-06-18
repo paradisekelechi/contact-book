a7585b162413730a5ba6b2bf9686948abd92bf1b
Paradise Kelechi <paradisekelechi@gmail.com>
2018-06-18 15:51:43 +0100

    task: implement views

--- baseApplication/views.py
+++ baseApplication/views.py
@@ -1,21 +1,39 @@
 from django.shortcuts import render
-
-
+from baseApplication.models import Contact
+from .forms import ContactForm
+from django.shortcuts import redirect
 # Create your views here.
+
+
 def home(request):
     return render(
         request,
-        'baseApplication/home.html'
+        'baseApplication/home.html',
+        {'contactCount': Contact.objects.count()}
     )
 
-def addContact(request):
+
+def add_contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.save()
+            return redirect('list_contacts')
+    else:
+        form = ContactForm()
     return render(
         request,
         'baseApplication/add-contact.html',
+        {'form': form}
     )
 
-def listContact(request):
+def list_contact(request):
+    allContacts = Contact.objects.all()
     return render(
         request,
         'baseApplication/list-contact.html',
+        {
+            'contacts': allContacts
+        }
     )
