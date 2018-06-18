1bf2da2a5066e52b25a06bebf63e491379ba798b
Paradise Kelechi <paradisekelechi@gmail.com>
2018-06-18 15:41:15 +0100

    add and implement templates

--- baseApplication/views.py
+++ baseApplication/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(
+        request,
+        'baseApplication/home.html'
+    )
+
+def addContact(request):
+    return render(
+        request,
+        'baseApplication/add-contact.html',
+    )
+
+def listContact(request):
+    return render(
+        request,
+        'baseApplication/list-contact.html',
+    )
